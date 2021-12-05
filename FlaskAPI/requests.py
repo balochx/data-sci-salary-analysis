8409312379a4ceed0c27bb2865e21dfb7a8154bb
balochx <alishayzadag@gmail.com>
2021-12-05 23:45:54 +0500

    added flask API endpoint

---
+++ FlaskAPI/requests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  5 15:25:23 2021
+
+@author: alishayzadag
+"""
+
+import requests 
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data) 
+
+r.json()
